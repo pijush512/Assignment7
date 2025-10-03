cf46a00e12242c2db341a97916a6df9754e5197f
pijush512 <psagorsarker512@gmail.com>
2025-10-03 21:37:46 +0600

    footer done

--- src/Component/Cards/Cards.jsx
+++ src/Component/Cards/Cards.jsx
@@ -6,17 +6,17 @@ const Cards = ({cardPromise, onCardClick, tasks}) => {
     const cardsData = use(cardPromise);
     return (
       <>
-        <div className='max-w-[1200px] mx-auto flex justify-between gap-4'>
-          <div className="w-[75%]">
+        <div className='max-w-[1200px] mx-auto flex flex-col md:flex-row items-center md:items-start justify-between gap-4'>
+          <div className="w-[90%] md:w-[75%]">
             <h3 className="font-bold text-xl mt-5 mb-5">Customer Tickets</h3>
-            <div className="  grid grid-cols-2 gap-5">
+            <div className="  grid grid-cols-1 md:grid-cols-2 gap-5">
               {cardsData.map((card) => (
                 <Card card={card} onCardClick={onCardClick}></Card>
               ))}
             </div>
           </div>
 
-          <div className='w-[25%]'>
+          <div className='w-[90%] md:w-[25%]'>
             <h3 className="font-bold text-xl mt-5 mb-5">Task Status</h3>
             <TaskStatus tasks={tasks}></TaskStatus>
           </div>
--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,10 +1,69 @@
 import React from 'react';
+import x from '../../assets/x.png'
+import linkedIn from '../../assets/in.png'
+import faceBook from '../../assets/fb.png'
+import gmail from '../../assets/fax.png'
 
 const Footer = () => {
     return (
-        <div>
-            <div>
-                
+        <div className=' bg-black'>
+            <div className='max-w-[1200px] mx-auto flex flex-col items-center md:items-start md:flex-row justify-between '>
+                <div className='py-10'>
+                    <h2 className='font-bold text-lg text-white'>CS — Ticket System</h2>
+                    <p className='mt-3 text-[#A1A1AA]'>Lorem Ipsum is simply dummy text of the <br /> printing and typesetting industry. Lorem <br /> Ipsum has been the industry's standard <br /> dummy text ever since the 1500s, when an <br /> unknown printer took a galley of type and <br /> scrambled it to make a type specimen book.</p>
+                </div>
+
+                <div className='py-10'>
+                    <h2 className='font-semibold text-white'>Company</h2>
+                    <ul className='mt-3 text-[#A1A1AA] '>
+                        <li className='mb-2'>About Us</li>
+                        <li className='mb-2'>Our Mission</li>
+                        <li>Contact Saled</li>
+                    </ul>
+                </div>
+
+                <div className='py-10'>
+                    <h2 className='font-semibold text-white'>Services</h2>
+                    <ul className='mt-3 text-[#A1A1AA] '>
+                        <li className='mb-2'>Products & Services</li>
+                        <li className='mb-2'>Customer Stories</li>
+                        <li className='mb-2'>Download Apps</li>
+                    </ul>
+                </div>
+
+                <div className='py-10'>
+                    <h2 className='font-semibold text-white'>Information</h2>
+                    <ul className='mt-3 text-[#A1A1AA] '>
+                        <li className='mb-2'>Privacy Policy</li>
+                        <li className='mb-2'>Terms & Conditions</li>
+                        <li className='mb-2'>Join Us</li>
+                    </ul>
+                </div>
+
+                <div className='py-10'>
+                    <h2 className='font-semibold text-white'>Social Links</h2>
+                    <ul className='mt-3 text-[#A1A1AA] '>
+                        <li className='flex items-center gap-2'>
+                            <img src={x} alt="" />
+                            @CS — Ticket System</li>
+
+                        <li className='flex items-center gap-2 mt-3'>
+                            <img src={linkedIn} alt="" />
+                            @CS — Ticket System</li>
+
+                        <li className='flex items-center gap-2 mt-3'>
+                            <img src={faceBook} alt="" />
+                            @CS — Ticket System</li>
+
+                        <li className='flex items-center gap-2 mt-3'>
+                            <img src={gmail} alt="" />
+                            @CS — Ticket System</li>
+                    </ul>
+                </div>
+            </div>
+            <div className='max-w-[1200px] mx-auto'>
+                <hr />
+                <p className='text-center mt-3 text-[#A1A1AA] pt-2 pb-5'>© 2025 CS — Ticket System. All rights reserved.</p>
             </div>
         </div>
     );
